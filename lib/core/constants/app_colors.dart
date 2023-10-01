e62f4168339fba3abc1cd81f2b4c27b4e8afc399
Dinesh Bala <baladnes@gmail.com>
2023-10-01 08:47:42 +0545

    Added login screen

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xFFED7E08);
+  static const Color secondaryColor = Color(0xFF081AED); // Complementary color
+  static const Color accentColor1 = Color(0xFF08EDA4); // Analogous color
+  static const Color accentColor2 = Color(0xFFED1C08); // Analogous color
+  static const Color accentColor3 = Color(0xFF7E08ED); // Triadic color
+}
---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:vcamp/core/constants/app_colors.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Spacer(),
+            const Text(
+              "Hello, VCampers",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
+            ),
+            const SizedBox(height: 32),
+            SvgPicture.asset(
+              "assets/diet_plan.svg",
+              height: 200,
+            ),
+            const SizedBox(height: 24),
+            const Text(
+              "Vcamper: Where Flavor Meets Functionality! Discover recipes, plan meals, and conquer the grocery store effortlessly. Your culinary adventure begins here",
+              textAlign: TextAlign.center,
+            ),
+            const Spacer(),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: AppColors.primaryColor,
+                fixedSize: const Size(double.maxFinite, 50),
+                elevation: 0,
+              ),
+              child: const Text("Sign in with Google"),
+            ),
+            const SizedBox(height: 8),
+          ],
+        ),
+      ),
+    );
+  }
+}
